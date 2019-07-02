8da2d0aa05c78b01e3f8f00b689af5e52c3fb8ce
Lipestroyer <lipe.antunes.2011@gmail.com>
2019-07-02 00:27:56 -0300

    commit

---
+++ modulo2/src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '9c3690053387174337d1e56df5df5769',
+  expiresIn: '7d',
+};
--- modulo2/src/routes.js
+++ modulo2/src/routes.js
@@ -1,10 +1,17 @@
 import { Router } from 'express';
 // import User from './app/models/User';
 import Usercontroller from './app/controllers/UserController';
+import Sessioncontroller from './app/controllers/SessionController';
+import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 
 routes.post('/users', Usercontroller.store);
+routes.post('/sessions', Sessioncontroller.store);
+
+routes.use(authMiddleware);
+
+routes.put('/users', Usercontroller.update);
 
 // routes.get('/', async (req, res) => {
 //   const user = await User.create({
