57964fa782af48e9c99c7a910d365ca2f90dd663
Lipestroyer <lipe.antunes.2011@gmail.com>
2019-07-14 16:53:03 -0300

    implementado o cancelamento do appointment

--- modulo2/src/app/controllers/AppointmentController.js
+++ modulo2/src/app/controllers/AppointmentController.js
@@ -1,5 +1,5 @@
 import * as Yup from 'yup';
-import { parseISO, isBefore, startOfHour, format } from 'date-fns';
+import { parseISO, isBefore, startOfHour, format, subHours } from 'date-fns';
 import ptBr from 'date-fns/locale/pt-BR';
 import Appointment from '../models/Appointment';
 import User from '../models/User';
@@ -98,6 +98,35 @@ class AppointmentController {
       content: `Novo agendamento de ${user.name} para ${formattedDate}`,
       user: provider_id,
     });
+
+    return res.json(appointment);
+  }
+
+  async delete(req, res) {
+    const appointment = await Appointment.findByPk(req.params.id);
+
+    if (appointment.user_id !== req.userId) {
+      return res.status(401).json({
+        error: "You don't have permission to cancel this appointment.",
+      });
+    }
+    const dateWithSub = subHours(appointment.date, 2);
+
+    if (appointment.canceled_at) {
+      return res.status(401).json({
+        error: 'appointment already cancelled!',
+      });
+    }
+
+    if (isBefore(dateWithSub, new Date())) {
+      return res
+        .status(401)
+        .json({ error: 'You can only cancel appointments 2 hours in advance' });
+    }
+
+    appointment.canceled_at = new Date();
+
+    await appointment.save();
 
     return res.json(appointment);
   }
