f6f29f13b87d1db5b506aa46530783188a472b65
Lipestroyer <lipe.antunes.2011@gmail.com>
2019-07-14 14:21:02 -0300

    Criado appointments e between date

--- modulo2/src/routes.js
+++ modulo2/src/routes.js
@@ -1,14 +1,15 @@
 import { Router } from 'express';
 import multer from 'multer';
 import multerConfig from './config/multer';
-// import User from './app/models/User';
+
 import Usercontroller from './app/controllers/UserController';
 import Sessioncontroller from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
+import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
 
 import authMiddleware from './app/middlewares/auth';
-import AppointmentController from './app/controllers/AppointmentController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -25,15 +26,8 @@ routes.get('/providers', ProviderController.index);
 routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
 
-routes.post('/files', upload.single('file'), FileController.store);
+routes.get('/schedule', ScheduleController.index);
 
-// routes.get('/', async (req, res) => {
-//   const user = await User.create({
-//     name: 'Philipe Antunes',
-//     email: 'philipe@teste.com.br',
-//     password_hash: '123123123123',
-//   });
-//   return res.json(user);
-// });
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
