454b29b5e04ac9605a248fcefcb04eef64270583
Lipestroyer <lipe.antunes.2011@gmail.com>
2019-07-25 01:03:01 -0300

    Criado projeto ReactNative

---
+++ modulo6/src/pages/Main/index.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import api from '../../services/api';
+
+import { Container, Form, Input, SubmitButton } from './styles';
+
+// import { Container } from './styles';
+
+export default class Main extends Component {
+  state = {
+    users: [],
+    newUser: '',
+  };
+
+  handleAddUser = async () => {
+    const { users, newUser } = this.state;
+
+    console.tron.log(newUser);
+    const response = await api.get(`/users/${newUser}`);
+    console.tron.log(JSON.stringify(response.data));
+    const data = {
+      name: response.data.name,
+      login: response.data.login,
+      bio: response.data.bio,
+      avatar: response.data.avatar_url,
+    };
+
+    this.setState({
+      users: [...users, data],
+      newUser: '',
+    });
+  };
+
+  render() {
+    const { users, newUser } = this.state;
+    return (
+      <Container>
+        <Form>
+          <Input
+            autoCorrect={false}
+            autoCaptalize="none"
+            placeholder="Adicionar usuário"
+            value={newUser}
+            onChangeText={text => this.setState({ newUser: text })}
+            returnKeyType="send"
+            onSubmitEditing={this.handleAddUser}
+          />
+          <SubmitButton onPress={this.handleAddUser}>
+            <Icon name="add" size={20} color="#FFF" />
+          </SubmitButton>
+        </Form>
+      </Container>
+    );
+  }
+}
+
+Main.navigationOptions = {
+  title: 'Usuários',
+};
